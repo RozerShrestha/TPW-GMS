0d2b161930d820add4279eef7d9e96aebd544409
Rozer Shrestha <rozer.shrestha611@gmail.com>
2023-04-23 02:45:50 +0545

    guest qr bulk created

--- TPW_GMS/test.aspx.cs
+++ TPW_GMS/test.aspx.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TPW_GMS.Data;
 using TPW_GMS.Models;
+using TPW_GMS.Services;
 
 namespace TPW_GMS
 {
@@ -13,21 +18,7 @@ namespace TPW_GMS
         string roleId, loginUser, splitUser;
         protected void Page_Load(object sender, EventArgs e)
         {
-             LoginUserInfo l = checkSession();
-            roleId = l.roleId;
-            loginUser = l.loginUser;
-            splitUser = l.splitUser;
-            if (!IsPostBack)
-            {
-                try
-                {
-                }
-                catch (Exception)
-                {
-
-                }
-
-            }
+            //CreateNewGuestQR();
         }
         public LoginUserInfo checkSession()
         {
@@ -44,6 +35,35 @@ namespace TPW_GMS
             uInfo.splitUser = splitUser[0];
 
             return uInfo;
+        }
+        public void CreateNewGuestQR()
+        {
+            using (TPWDataContext db = new TPWDataContext())
+            {
+                var guestList = db.Guests.ToList();
+                foreach (var guest in guestList)
+                {
+                    var qrTextEncrypted = Service.EncryptData(guest.email);
+                    //var em = JsonConvert.DeserializeObject<Guest>(qrText);
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrTextEncrypted, QRCodeGenerator.ECCLevel.Q);
+                    System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+                    imgBarCode.Height = 150;
+                    imgBarCode.Width = 150;
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    using (Bitmap bitMap = qrCode.GetGraphic(20))
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                            byte[] byteImage = ms.ToArray();
+                            System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                            img.Save(Server.MapPath(@"~\Image\Guests\") + guest.email + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                            imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                        }
+                    }
+                }
+            }
         }
     }
 }
