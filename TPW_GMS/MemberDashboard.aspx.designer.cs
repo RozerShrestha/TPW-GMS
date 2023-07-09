69c386d2d0d3e47db9febe5d0ecd4bdbb7f1fc82
Rozer Shrestha <rozer.shrestha611@gmail.com>
2023-07-10 00:22:02 +0545

    memberdashboard removed from master page

--- TPW_GMS/MemberDashboard.aspx.designer.cs
+++ TPW_GMS/MemberDashboard.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace TPW_GMS
 
     public partial class MemberDashboard
     {
-
-        /// <summary>
-        /// hidHeader control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField hidHeader;
     }
 }
